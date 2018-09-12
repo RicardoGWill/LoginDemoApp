97f15d4fd9c292181b971647b95a317860100611
Ricardo Willems <RicardoGWill@gmail.com>
2018-09-11 22:57:37 -0400

    This time, I added functionality to the "UpdateProfileActivity.java" page.  Note that it is necessary to refresh (go back to) the screen to see the changes for the image, and a problem occurs when updating the rest of the information.  However, it is updated.

--- app/src/main/java/com/ricardogwill/logindemoapp/UpdateProfileActivity.java
+++ app/src/main/java/com/ricardogwill/logindemoapp/UpdateProfileActivity.java
@@ -1,5 +1,9 @@
 package com.ricardogwill.logindemoapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,14 +14,24 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public class UpdateProfileActivity extends AppCompatActivity {
+
+    // Notes for further improvement: "Swipe Refresh" and "Image Caching".
 
     private ImageView updateImageView;
     private EditText updateNameEditText, updateEmailEditText, updateAgeEditText;
@@ -25,7 +39,27 @@ public class UpdateProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
     private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
+    // Everything down to the bottom of the "onActivityResult" method was copied (and then slightly modified)
+    // from "RegistrationActivity.java".
+    private static int PICK_IMAGE = 123;
+    Uri imagePath;
+    private StorageReference storageReference;
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK && data.getData() != null) {
+            imagePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imagePath);
+                // "updateImageView" is used because it is the name of our ImageView in this Activity.
+                updateImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +78,7 @@ public class UpdateProfileActivity extends AppCompatActivity {
         // but the "setText" parts and the Toast have been changed.
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
 
         final DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
 
@@ -62,6 +97,20 @@ public class UpdateProfileActivity extends AppCompatActivity {
             }
         });
 
+        // Most of this next major block is copied from "ProfileActivity.java".
+        final StorageReference storageReference = firebaseStorage.getReference();
+        // Get the image stored on Firebase via "User id/Images/Profile Pic.jpg".
+        storageReference.child(firebaseAuth.getUid()).child("Images").child("Profile Pic").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                // Using "Picasso" (http://square.github.io/picasso/) after adding the dependency in the Gradle.
+                // ".fit().centerInside()" fits the entire image into the specified area.
+                // Finally, add "READ" and "WRITE" external storage permissions in the Manifest.
+                // "updateImageView" is used, as it is the ImageView in this Activity.
+                Picasso.get().load(uri).fit().centerInside().into(updateImageView);
+            }
+        });
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,9 +122,34 @@ public class UpdateProfileActivity extends AppCompatActivity {
 
                 databaseReference.setValue(userProfile);
 
+                // This next block is copied (and then altered) from within the "sendUserData()" function in "RegistrationActivity.java".
+                StorageReference imageReference = storageReference.child(firebaseAuth.getUid()).child("Images").child("Profile Pic"); //User id/Images/Profile Pic.jpg
+                UploadTask uploadTask = imageReference.putFile(imagePath);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(UpdateProfileActivity.this, "Upload Failed.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(UpdateProfileActivity.this, "Upload Successful.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
                 finish();
+            }
+        });
 
-
+        updateImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // The following 4 lines of code are copied from an onClickListener in "RegistrationActivity.java".
+                Intent profileIntent = new Intent();
+                // Type can be "image/*", "application/*", "audio/*"
+                profileIntent.setType("image/*");
+                profileIntent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(profileIntent, "Select Image."), PICK_IMAGE);
             }
         });
 
